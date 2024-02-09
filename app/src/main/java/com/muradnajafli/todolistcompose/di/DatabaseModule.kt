5efaa1649e962045437ae7d3ca769a62609f9360
Murad Najafli <muradnecefli05@gmail.com>
2024-02-10 01:50:49 +0400

    - First Commit

--- app/src/main/java/com/muradnajafli/todolistcompose/di/DatabaseModule.kt
+++ app/src/main/java/com/muradnajafli/todolistcompose/di/DatabaseModule.kt
@@ -1,14 +1,12 @@
 package com.muradnajafli.todolistcompose.di
 
 import android.app.Application
-import android.content.Context
 import androidx.room.Room
 import com.muradnajafli.todolistcompose.data.database.ToDoDao
 import com.muradnajafli.todolistcompose.data.database.ToDoDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
